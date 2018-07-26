bfc71321b1f300cb423a8811ce09042971a148cd
indianajim <jimrunkel@gmail.com>
2018-07-26 13:35:22 -0400

    Add project files.

---
+++ WebReviews/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebReviews.Startup))]
+namespace WebReviews
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
