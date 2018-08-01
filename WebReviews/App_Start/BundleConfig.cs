0d350d98c4e52ecbbc45f4e24fd9e60fffd0aac0
indianajim <jimrunkel@gmail.com>
2018-07-31 23:42:54 -0400

    updating display steps

--- WebReviews/App_Start/BundleConfig.cs
+++ WebReviews/App_Start/BundleConfig.cs
@@ -8,6 +8,12 @@ namespace WebReviews
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundle/otf").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js",
+                "~/Scripts/jquery-unobtrusive.js",
+                "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
