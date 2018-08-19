c764905f89cbd4d16ec1376beaeb9ce766433ad4
indianajim <jimrunkel@gmail.com>
2018-08-19 19:32:03 -0400

    further build

--- WebReviews/Controllers/HomeController.cs
+++ WebReviews/Controllers/HomeController.cs
@@ -9,6 +9,19 @@ namespace WebReviews.Controllers
     {
         WebReviewsDB _db = new WebReviewsDB();
 
+        public ActionResult Autocomplete(string term)
+        {
+            var model =
+                _db.Restaurants
+                .Where(r => r.Name.StartsWith(term))
+                .Take(10)
+                .Select(r => new
+                {
+                    label = r.Name
+                });
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Index(string searchTerm = null)
         {
 
@@ -26,6 +39,11 @@ namespace WebReviews.Controllers
                         CountOfReviews = r.Reviews.Count()
                     }
                     );
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Restaurants", model);
+            }
 
             return View(model);
         }
